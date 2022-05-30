079d15d02fffd5d5dd83d4017ffa99ccec089a9b
loisiling <silingloi@hotmail.com>
2022-05-30 11:35:04 +0800

    test deploy

---
+++ src/config/index.js
@@ -0,0 +1,17 @@
+//Contains configuration details
+const env = process.env.REACT_APP_ENV;
+
+let config = {
+    baseURL:"https://localhost"
+}
+
+switch (env.toUpperCase()){
+    case "STAGE":
+        config.baseURL = "https://stage.skillsunion.com"
+        break;
+    case "PRODUCTION":
+        config.baseURL = "https://skillunion.com"
+        break;
+}
+
+export default config;
